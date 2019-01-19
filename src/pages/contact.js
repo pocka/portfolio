0d98f3f7efb6f3698350ae8f46b59d561ae6fea1
pocka <pockawoooh@gmail.com>
2019-01-19 14:41:28 +0900

    :+1: Fix spacing problem

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,7 @@ export const render = container => {
       <my-balloons>
         <my-balloon>
           If you have any questions,<br/>
-          please email to
+          please email to&nbsp;
           <a href="mailto:pockawoooh@gmail.com" target="_blank" rel="noopener">
             pockawoooh@gmail.com
           </a>
