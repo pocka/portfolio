347443ecb2504f9fbef49edaf1661f18db19a3dd
pocka <pockawoooh@gmail.com>
2019-01-21 15:45:13 +0900

    :sparkles: Add items for skill page

--- src/pages/skill/index.js
+++ src/pages/skill/index.js
@@ -1,7 +1,10 @@
 import '~/components/my-explainer'
 
 import jsSVG from './js.svg'
+import nuxtSVG from './nuxt.svg'
 import reactSVG from './react.svg'
+import sassSVG from './sass.svg'
+import tsSVG from './ts.svg'
 import vueSVG from './vue.svg'
 import webComponentsSVG from './web-components.svg'
 import webpackSVG from './webpack.svg'
@@ -24,6 +27,20 @@ export const render = container => {
           </my-balloon>
           <my-balloon>
             Tends to write in functional style.
+          </my-balloon>
+        </my-balloons>
+        ${icon(tsSVG)}
+        <span slot="label">TypeScript</span>
+        <my-balloons>
+          <my-balloon>
+            Using TypeScript since 2016.
+          </my-balloon>
+          <my-balloon pop-direction="right">
+            Focus on clean type declaration and abstraction.
+          </my-balloon>
+          <my-balloon>
+            My main usages are writing libraries and<br/>
+            building complex applications.
           </my-balloon>
         </my-balloons>
         ${icon(reactSVG)}
@@ -53,6 +70,20 @@ export const render = container => {
             </a>.
           </my-balloon>
         </my-balloons>
+        ${icon(nuxtSVG)}
+        <span slot="label">Nuxt.js</span>
+        <my-balloons>
+          <my-balloon>
+            Nuxt.js boosts my application prototype<br/>
+            and website creation process.
+          </my-balloon>
+          <my-balloon pop-direction="right">
+            I'm good at building static pages
+            using&nbsp;<code>nuxt generate</code><br/>
+            with contents management system such as headless CMS<br/>
+            or local JSON files.
+          </my-balloon>
+        </my-balloons>
         ${icon(webComponentsSVG)}
         <span slot="label">Web Components</span>
         <my-balloons>
@@ -64,6 +95,20 @@ export const render = container => {
             I write articles, especially for this.<br/>
             There are so many outdated or invalid articles/posts,<br/>
             this situation is too bad for everyone, IMO.
+          </my-balloon>
+        </my-balloons>
+        ${icon(sassSVG)}
+        <span slot="label">Sass</span>
+        <my-balloons>
+          <my-balloon>
+            My primary styling language.
+          </my-balloon>
+          <my-balloon pop-direction="right">
+            Former ".sass" user, but moved to ".scss"...
+          </my-balloon>
+          <my-balloon>
+            I'm not a fan of&nbsp;<code>node-sass</code>.<br/>
+            Let's go with&nbsp;<code>sass</code>&nbsp;package FTW.
           </my-balloon>
         </my-balloons>
         ${icon(webpackSVG)}
